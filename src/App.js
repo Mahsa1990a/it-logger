1366d7136613bb7aa0d96c3a3f69305dc81231fb
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-10 18:35:38 +0000

    added useEffect for running Materialize

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from "react";
+
 // Materialize:
 import 'materialize-css/dist/css/materialize.min.css'; //import materialize
 import M from 'materialize-css/dist/js/materialize.min.js';// import js in case for doing modals and ...
@@ -5,6 +7,12 @@ import M from 'materialize-css/dist/js/materialize.min.js';// import js in case 
 import './App.css';
 
 const App = () => {
+
+  useEffect(() => {
+    // This initializes Materialize JavaScript:
+    M.AutoInit(); //AutoInit is a func, when we install Materialize, we can use it like this
+  }, []);
+
   return (
     <div className="App">
       My App
