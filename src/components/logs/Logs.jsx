b138ec5097605ff5d27317fc82acacd2a0fe8f2c
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-11 14:43:38 +0000

    added getLogs func and requset from backend with fetch

--- src/components/logs/Logs.jsx
+++ src/components/logs/Logs.jsx
@@ -3,6 +3,21 @@
 import React, { useState, useEffect } from 'react'; 
 
 const Logs = () => {
+
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getLogs = async () => {
+    setLoading(true);
+    // Make a request from backend(using fetch instead of axios):
+    const res = await fetch('/logs'); //fetch returns a promiss // becuase we added proxy we dont need to say http://localhost:5000/logs
+    // Id doesnt return data right away, we have to format it as json:
+    const data = await res.json();
+
+    setLogs(data); // Set the logs to data
+    setLoading(false);
+  };
+
   return (
     <div>
       
