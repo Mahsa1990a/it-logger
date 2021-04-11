531f77183da48ee476fd6b931bd8f23cdf827ce6
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-11 14:35:22 +0000

    imported useState and useEffect

---
+++ src/components/logs/Logs.jsx
@@ -0,0 +1,13 @@
+//useState: because we're gonna store the logs in our component level state and later we'll move it to redux
+// useEffect: we wanna be able to make our request
+import React, { useState, useEffect } from 'react'; 
+
+const Logs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Logs;
