e490ff8fe2491615eeff26daf125c94b06c91174
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-12 17:42:35 +0000

    aaded id and technician

--- src/components/logs/LogItem.jsx
+++ src/components/logs/LogItem.jsx
@@ -7,6 +7,12 @@ const LogItem = ({ log }) => {
     <li className="collection-item">
       <div>
         <a href="#edit-log-model" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>{log.message}</a>
+        <br/>
+        <span className="grey-text">
+          <span className="black-text">ID #{log.id}</span>
+          last updated by {' '}
+          <span className="black-text">{log.tech}</span>
+        </span>
       </div>
     </li>
   )
