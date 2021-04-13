51fb040e0a8914d71210dbe87250d5914449bfa9
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-13 17:39:21 +0000

    added logActions

---
+++ src/actions/logActions.js
@@ -0,0 +1,13 @@
+// Any actions
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+
+export const getLogs = () => {
+
+}
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
