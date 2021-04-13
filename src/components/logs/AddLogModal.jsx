319d5a51b93a6c3155846d3c561de4457d6840bc
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-13 13:34:52 +0000

    added className and rulls of materialize

--- src/components/logs/AddLogModal.jsx
+++ src/components/logs/AddLogModal.jsx
@@ -10,7 +10,15 @@ const AddLogModal = () => {
   return (
     // It's gonna be a modal so div will have id which this id is match with href of add btn in the AddBtn.jsx:
     <div id='add-log-modal' className="modal" style={modalStyle}>
-      
+      <div className="modal-content">
+        <h4>Enter System Log</h4>
+        <div className="row">
+          <div className="input-field">
+            <input type="text" name="message" value={message} onChange={e => setMessage(e.target.value)} />
+            <label htmlFor="message" className="active">Log Message</label>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
