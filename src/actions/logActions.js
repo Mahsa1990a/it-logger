f4fa909729372083f01bf2e5782ca51af46368b4
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-13 19:25:33 +0000

    used SET_LOADING and GET_LOGS

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -2,8 +2,18 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 
 export const getLogs = () => {
+  return async (dispatch) => {
+    setLoading();
 
-}
+    const res = await fetch('/logs');
+    const data = res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  };
+};
 
 // Set loading to true
 export const setLoading = () => {
