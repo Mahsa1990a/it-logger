415ae4f152e9bb3e716d316eb8c4b424c7008cad
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-13 19:31:58 +0000

    added case for LOGS_ERROR

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -15,6 +15,12 @@ export default(state = initialState, action) => {
         ...state,
         loading: true
       };
+    case LOGS_ERROR:
+      console.error(action.payload)
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
