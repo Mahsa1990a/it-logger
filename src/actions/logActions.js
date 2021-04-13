f66e66d023c983e2b68f15a2b26cb023e2ca0fc7
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-13 19:29:31 +0000

    refactorde getLogs and added err handler

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,19 +1,41 @@
 // Any actions
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 
-export const getLogs = () => {
-  return async (dispatch) => {
-    setLoading();
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
 
+//     const res = await fetch('/logs');
+//     const data = res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data
+//     });
+//   };
+// };        REFACTOR TO:
+
+export const getLogs = () => async dispatch => {
+
+  try {
+    setLoading();
+  
     const res = await fetch('/logs');
     const data = res.json();
-
+  
     dispatch({
       type: GET_LOGS,
       payload: data
     });
-  };
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+
 };
+
 
 // Set loading to true
 export const setLoading = () => {
