9b677bb77eb027200fe72820294327a82f7edbff
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-13 13:31:14 +0000

    added id and className

--- src/components/logs/AddLogModal.jsx
+++ src/components/logs/AddLogModal.jsx
@@ -6,12 +6,18 @@ const AddLogModal = () => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
-  
+
   return (
-    <div>
+    // It's gonna be a modal so div will have id which this id is match with href of add btn in the AddBtn.jsx:
+    <div id='add-log-modal' className="modal" style={modalStyle}>
       
     </div>
   )
-}
+};
+
+const modalStyle = {
+  width: '75%',
+  height: '75%'
+};
 
 export default AddLogModal;
