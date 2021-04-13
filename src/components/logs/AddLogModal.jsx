8f64886674b5fa08461df0e60b2e7ceaba484017
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-13 13:27:30 +0000

    Created AddLogoModal component and import useState

---
+++ src/components/logs/AddLogModal.jsx
@@ -0,0 +1,17 @@
+// This is gonna be a form so we'll have some component level state
+import React, { useState } from 'react';
+
+const AddLogModal = () => {
+
+  const [message, setMessage] = useState('');
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState('');
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddLogModal;
