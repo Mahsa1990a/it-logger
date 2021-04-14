4488ca521ddf43e51354c096164a34116c8d71e0
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-14 14:35:08 +0000

    added getTechs and setLoading

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -1,2 +1,32 @@
 // ANy action with technicians would be here
 import { GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR } from "./types";
+
+// Get techs from server
+export const getTechs = () => async dispatch => {
+
+  try {
+    setLoading();
+  
+    const res = await fetch('http://localhost:5000/techs');
+    const data = await res.json();
+  
+    dispatch({
+      type: GET_TECHS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.message
+    });
+  }
+
+};
+
+
+// Set loading to true -> copied from logActions
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
