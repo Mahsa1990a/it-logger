19ba9125eb236337f457fbb69ee9aaa85d1ff815
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-14 14:27:09 +0000

    added techReducer and import it into main route index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 // We'll bring any other reducers we have
 import { combineReducers } from 'redux';
 import logReducer from './logReducer';
+import techReducer from './techReducer';
 
 export default combineReducers({  // It takes an Object with all reducers
-  log: logReducer
+  log: logReducer,
+  tech: techReducer
 });
