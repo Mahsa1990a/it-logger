1ff6b55b7cd0bc5de42c9b38e889b49257d4c087
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-14 00:18:17 +0000

    added case for SEARCH_LOGS

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, UPDATE_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT } from '../actions/types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, UPDATE_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, SEARCH_LOGS } from '../actions/types';
 
 const initialState = {
   logs: null,
@@ -25,6 +25,11 @@ export default(state = initialState, action) => {
       return {
         ...state,
         logs: state.logs.map(log => log.id === action.payload.id ? action.payload : log)
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
       };
     case DELETE_LOG:
       return {
