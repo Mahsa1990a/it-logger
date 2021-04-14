fc3fb3390c31e5ce1bc7d14ee593b42264ab02cf
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-14 15:20:18 +0000

    imported useEfferct because we need to call getTechs when component loads

--- src/components/techs/TechSelectOptions.jsx
+++ src/components/techs/TechSelectOptions.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react'; // we need to call getTechs when component loads, so we need useEffect
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getTechs } from '../../actions/techActions';
 
 const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
+
+  useEffect(() => {
+    getTechs();
+    // eslint-disable-next-line
+  }, []); //We only want to run it when component mounts so []
+
   return (
     <div>
       
